6f5f2ae4002d15d98c5dd7d852cf61ad409419f7
김윤홍 <yunhongkim123@gmail.com>
2024-10-23 09:07:10 +0900

    :sparkles: feat : basic structure

---
+++ a602/src/test/java/com/MOA/a602/A602ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MOA.a602;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class A602ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
