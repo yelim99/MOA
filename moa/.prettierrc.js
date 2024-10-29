a0319d02c3633976ebdc20e787ecde4b3f129e98
inception010203 <inception010203@gmail.com>
2024-10-29 17:54:20 +0900

    :sparkles:[Feat] #S11P31A602-152 #in-progress Prettier 설정 생성

--- moa/.prettierrc.js
+++ moa/.prettierrc.js
@@ -1,7 +1,9 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'all',
+  arrowParens: 'avoid', // 화살표 함수의 매개변수가 하나일 경우 괄호 생략
+  // bracketSameLine: true,
+  bracketSpacing: false, // 중괄호와 내부 텍스트 사이에 공백 추가
+  singleQuote: true, // 작은 따옴표 사용
+  trailingComma: 'all', // 여러 줄로 나열된 데이터에 마지막 쉼표를 추가
+  tabWidth: 2, // 탭 간격을 2칸으로 설정
+  semi: true, // 세미콜론 사용
 };
