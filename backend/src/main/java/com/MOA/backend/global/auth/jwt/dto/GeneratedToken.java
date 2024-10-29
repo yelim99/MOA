d31d0f992e80d3e3c1b64a492d1666f5e0cc6d4f
김윤홍 <yunhongkim123@gmail.com>
2024-10-29 17:50:43 +0900

    ✨[Feat] #S11P31A602-190 #in-progress login 구현

---
+++ backend/src/main/java/com/MOA/backend/global/auth/jwt/dto/GeneratedToken.java
@@ -0,0 +1,12 @@
+package com.MOA.backend.global.auth.jwt.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@AllArgsConstructor
+public class GeneratedToken {
+    private String accessToken;
+}
--- backend/src/main/java/com/MOA/backend/global/config/SecurityConfig.java
+++ backend/src/main/java/com/MOA/backend/global/config/SecurityConfig.java
@@ -1,12 +1,24 @@
 package com.MOA.backend.global.config;
 
-import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-@RequiredArgsConstructor
 public class SecurityConfig {
-//    private final
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/api/user/signup", "/api/user/login").permitAll()  // 회원가입, 로그인 경로 예외
+                        .anyRequest().authenticated()  // 다른 요청은 인증 필요
+                )
+                .oauth2Login();  // OAuth2 로그인 설정을 유지
+
+        return http.build();
+    }
 }
