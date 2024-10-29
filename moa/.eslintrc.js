e9902566bb7827f2c318bf81e2438c81984aeb1f
inception010203 <inception010203@gmail.com>
2024-10-29 17:05:43 +0900

    :sparkles:[Feat] #S11P31A602-150 #in-progress EsLint에 Prettier 설정 추가

--- moa/.eslintrc.js
+++ moa/.eslintrc.js
@@ -5,8 +5,9 @@ module.exports = {
     '@react-native', // React Native 프로젝트에 적합한 기본 ESLint 규칙
     'plugin:@typescript-eslint/recommended', // TypeScript에 특화된 권장 규칙 추가
     'plugin:react/recommended', // React 관련 규칙 적용. 잘못된 JSX 작성이나 기타 문제를 방지
+    'plugin:prettier/recommended', // Prettier 설정 추가
   ],
-  plugins: ['@typescript-eslint', 'react'],
+  plugins: ['@typescript-eslint', 'react', 'prettier'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true, // JSX 구문을 파싱할 수 있도록 허용. 필수 옵션
@@ -19,6 +20,7 @@ module.exports = {
     'react/self-closing-comp': 'warn', // 내용이 없는 태그는 셀프 클로징 태그로 작성하라는 경고 표시
     'react/jsx-key': 'warn', // 리스트 렌더링 시 key 속성이 없는 경우 경고 표시
     // 'no-console': 'warn', // console.log와 같은 콘솔 사용을 경고
+    'prettier/prettier': 'warn', // Prettier 규칙을 따르지 않는 경우 경고
   },
   settings: {
     react: {
