20e4a3eb6c9838724ee604ee15eaab997699990c
dla9520 <dla9520@naver.com>
2024-10-31 17:53:44 +0900

    :sparkles:[Feat] #S11P31A602-180 #in-progress Navigation 수정

--- moa/App.tsx
+++ moa/App.tsx
@@ -1,37 +1,31 @@
 import React from 'react';
-import {ThemeProvider} from 'styled-components/native';
+import styled, {ThemeProvider} from 'styled-components/native';
 import {theme} from './src/styles/theme';
-import {SafeAreaView, Text, View} from 'react-native';
-import {GlobalStyles} from './src/styles/GlobalStyles';
-import styled from 'styled-components/native';
-import Test from './src/components/Test';
-import Navigation from './src/components/Navigation';
+import {SafeAreaView} from 'react-native';
+import Navigation from './src/components/common/Navigation';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Home from './src/screens/Home';
 import MyPage from './src/screens/MyPage';
-
-const Container = styled.View`
-  background-color: ${({theme}) => theme.colors.lightblue};
-  padding: 10px;
-`;
-const StyledText = styled.Text`
-  ${GlobalStyles};
-`;
+import {GlobalStyles} from './src/styles/GlobalStyles';
 
 const Tab = createBottomTabNavigator();
+
+const StyledSafeAreaView = styled.SafeAreaView`
+  ${GlobalStyles};
+`;
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <SafeAreaView style={{flex: 1}}>
+      <StyledSafeAreaView style={{flex: 1}}>
         <NavigationContainer>
           <Tab.Navigator tabBar={(props) => <Navigation {...props} />}>
             <Tab.Screen name="Home" component={Home} />
             <Tab.Screen name="MyPage" component={MyPage} />
           </Tab.Navigator>
         </NavigationContainer>
-      </SafeAreaView>
+      </StyledSafeAreaView>
     </ThemeProvider>
   );
 };
