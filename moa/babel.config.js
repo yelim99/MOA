ea2a31e29197aa91d67504c55a7d233d3d48ac1b
inception010203 <inception010203@gmail.com>
2024-11-01 17:46:39 +0900

    :sparkles:[Feat] #S11P31A602-216 #in-progress env라이브러리 설치, env 생성, babel config 설정, build.gradle 설정

--- moa/babel.config.js
+++ moa/babel.config.js
@@ -1,5 +1,14 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   // babel.config.js
-  plugins: ['react-native-reanimated/plugin'],
+  plugins: [
+    'react-native-reanimated/plugin',
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env',
+        path: '.env',
+      },
+    ],
+  ],
 };
