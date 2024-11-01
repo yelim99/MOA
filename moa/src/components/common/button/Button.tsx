9b4847219bb816d6039830498cf7d2e5e48ae51c
inception010203 <inception010203@gmail.com>
2024-11-01 10:14:17 +0900

    :sparkles:[Feat] #S11P31A602-211 #in-progress 버튼 공통 속성 설정

--- moa/src/components/common/button/Button.tsx
+++ moa/src/components/common/button/Button.tsx
@@ -1,36 +1,45 @@
 // src/components/common/button/Button.tsx
 import React from 'react';
-import {TouchableOpacityProps, StyleProp, ViewStyle} from 'react-native';
-import {StyledButton, ButtonText, ButtonIcon} from './Buttons.styles';
+import { TouchableOpacityProps, StyleProp, ViewStyle } from 'react-native';
+import { StyledButton, ButtonText, ButtonIcon } from './Buttons.styles'
 
 interface ButtonProps extends TouchableOpacityProps {
   text?: string;
   icon?: JSX.Element;
-  variant?: 'primary' | 'secondary' | 'outline' | 'icon' | 'rounded';
-  size?: 'small' | 'medium' | 'large';
+  content?: 'icon' | 'text' | 'colorSelect';
+  size?: 'small' | 'medium' | 'large'; // 텍스트 버튼의 사이즈 
+  backcolor?: 'white' | 'mainlightorange' | 'maindarkorange'; // 배경색
+  textcolor?: 'white' | 'maindarkorange'; // 글자색
   styleOverride?: StyleProp<ViewStyle>;
-  onPress?: () => void;
+  onPress?: () => void; //  기능
 }
 
 const Button: React.FC<ButtonProps> = ({
   text,
   icon,
-  variant = 'primary', // 기본값 설정
-  size = 'medium', // 기본값 설정
+  content = 'text', // 기본값 설정
+  size = 'small', // 기본값 설정
+  backcolor = 'white', // 기본값 설정
+  textcolor,
   styleOverride,
   onPress,
   ...props
 }) => {
   return (
     <StyledButton
-      variant={variant}
+      content={content}
       size={size}
+      backcolor={backcolor}
       style={styleOverride}
       onPress={onPress}
       {...props}
     >
       {icon && <ButtonIcon>{icon}</ButtonIcon>}
-      {text && <ButtonText variant={variant}>{text}</ButtonText>}
+      {content === 'text' && text && (
+        <ButtonText content='text' backcolor={backcolor}>
+          {text}
+        </ButtonText>
+      )}
     </StyledButton>
   );
 };
