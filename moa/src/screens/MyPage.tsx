1e145b0bbf934da100253049b4f6058bada54314
inception010203 <inception010203@gmail.com>
2024-11-04 13:38:36 +0900

    :recycle:[Refactor] #S11P31A602-263 #in-progress 유저 정보 컴포넌트 분리

--- moa/src/screens/MyPage.tsx
+++ moa/src/screens/MyPage.tsx
@@ -5,29 +5,12 @@ import styled, {css, useTheme, DefaultTheme} from 'styled-components/native';
 import {IconButton} from '../components/common/button/IconButton';
 import StyleSheet from 'styled-components/dist/sheet';
 import {TextButton} from '../components/common/button/TextButton';
+import MyInfo from '../components/mypage/MyInfo';
 
 interface TextProps {
   variant: 'title' | 'subtitle' | 'body';
   color?: string;
 }
-
-const MyInfo = styled.View`
-  border-radius: 15px;
-  elevation: 2;
-  height: 100px;
-  background-color: ${(props) => props.theme.colors.white};
-  margin: 10px 0;
-  padding: 30px;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-`;
-
-const TextInfo = styled.View`
-  flex-direction: column;
-  width: 200px;
-  color: ${(props) => props.theme.colors.black};
-`;
 
 const Texts = styled.Text<TextProps>`
   ${({variant, theme, color}) => {
@@ -54,19 +37,6 @@ const Texts = styled.Text<TextProps>`
         `;
     }
   }}
-`;
-const IconButtonStyled = styled.View`
-  position: absolute;
-  top: 8px;
-  right: 8px;
-`;
-
-const MyProfile = styled.Image`
-  border-radius: 50px;
-  border: 1px solid ${(props) => props.theme.colors.lightgray};
-  width: 70px;
-  height: 70px;
-  background-color: ${(props) => props.theme.colors.maindarkorange};
 `;
 
 const Face = styled.View`
@@ -98,29 +68,7 @@ const MyPage = (props: {theme: any}) => {
     <ScreenContainer>
       <View>
         <Texts variant="title">마이페이지</Texts>
-        <MyInfo>
-          <TextInfo>
-            <Texts variant="subtitle">
-              <Texts variant="subtitle" color={theme.colors.maindarkorange}>
-                {DummyData[0].name}
-              </Texts>
-              님 안녕하세요!
-            </Texts>
-            <Texts variant="body">내 그룹 | {DummyData[0].group} 개</Texts>
-            <Texts variant="body">
-              누적 다운로드 사진 수 | {DummyData[0].totalDownloads} 장
-            </Texts>
-          </TextInfo>
-          <MyProfile source={{uri: '/'}}></MyProfile>
-          <IconButtonStyled>
-            <IconButton
-              backcolor="white"
-              iconName="edit"
-              iconSet="Material"
-              onPress={() => console.log('편집 아이콘 버튼 눌렀음')}
-            ></IconButton>
-          </IconButtonStyled>
-        </MyInfo>
+        <MyInfo></MyInfo>
         <Face>
           <Texts variant="subtitle">얼굴 등록</Texts>
           <TextButton
