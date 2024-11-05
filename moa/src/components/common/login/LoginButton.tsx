b862598aaafdfb6feda7e769f7501d48c1bfb0a8
inception010203 <inception010203@gmail.com>
2024-11-05 10:58:03 +0900

    :sparkles:[Feat] #S11P31A602-155 #in-progress 로그인 버튼 기능 구현

--- moa/src/components/common/login/LoginButton.tsx
+++ moa/src/components/common/login/LoginButton.tsx
@@ -1,9 +1,30 @@
+// src/components/common/login/LoginButton.tsx
 import React from 'react';
+import {Button, Alert} from 'react-native';
+import {login as kakaoLogin} from '@react-native-seoul/kakao-login';
 
-const LoginButton = () => {
-    return(
-        
-    )
-}
+type LoginButtonProps = {
+  onLoginSuccess: (kakaoToken: string) => void;
+};
+
+const LoginButton: React.FC<LoginButtonProps> = ({onLoginSuccess}) => {
+  const handleKakaoLogin = async () => {
+    try {
+      const result = await kakaoLogin();
+      const kakaoToken = result.accessToken;
+
+      if (kakaoToken) {
+        onLoginSuccess(kakaoToken);
+      } else {
+        throw new Error('카카오 토큰 발급 못받음!');
+      }
+    } catch (error) {
+      console.error('카카오 로그인 실패', error);
+      Alert.alert('로그인 실패', '다시 시도해주세용');
+    }
+  };
+
+  return <Button title="Login with Kakao" onPress={handleKakaoLogin} />;
+};
 
 export default LoginButton;
