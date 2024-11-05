971795d3717e4cf0460ddecccf930f9ae60a1c50
dla9520 <dla9520@naver.com>
2024-11-05 09:29:42 +0900

    :art:[Design] #S11P31A602-282 #in-progress 나의 순간 생성 기본 레이아웃 구현

--- moa/src/screens/moment/MomentAdd.tsx
+++ moa/src/screens/moment/MomentAdd.tsx
@@ -1,11 +1,35 @@
-import {View, Text} from 'react-native';
-import React from 'react';
+import React, {useState} from 'react';
 import ScreenContainer from '../../components/common/ScreenContainer';
+import AddInputBox from '../../components/common/input/AddInputBox';
+import {Picker} from '@react-native-picker/picker';
+import styled from 'styled-components/native';
 
 const MomentAdd = () => {
+  const [momentName, setMomentName] = useState('');
+  const [momentDescription, setMomentDescription] = useState('');
+  const [momentOption, setMomentOption] = useState('');
+
   return (
     <ScreenContainer>
-      <Text>MomentAdd</Text>
+      <AddInputBox
+        label="순간 이름"
+        value={momentName}
+        onChangeText={(text) => setMomentName(text)}
+      />
+      <AddInputBox
+        label="순간 설명"
+        value={momentDescription}
+        onChangeText={(text) => setMomentDescription(text)}
+      />
+      <AddInputBox label="멤버 권한 설정" isText={false}>
+        <Picker
+          selectedValue={momentOption}
+          onValueChange={(itemValue) => setMomentOption(itemValue)}
+        >
+          <Picker.Item label="나의 업로드만 허용" value="0" />
+          <Picker.Item label="모든 멤버의 업로드 허용" value="1" />
+        </Picker>
+      </AddInputBox>
     </ScreenContainer>
   );
 };
