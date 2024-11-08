aac54e0c3b1b46290173520299937ce7de12c79b
inception010203 <inception010203@gmail.com>
2024-11-08 11:00:10 +0900

    :sparkles:[Feat] #S11P31A602-155 #in-progress 로그인 버튼 생성 및 테스트

--- moa/App.tsx
+++ moa/App.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable react/no-unstable-nested-components */
 import React, {useState, useEffect} from 'react';
-import React, {useState, useEffect} from 'react';
 import styled, {ThemeProvider} from 'styled-components/native';
 import {theme} from './src/styles/theme';
 import Navigation from './src/components/common/Navigation';
@@ -21,10 +20,8 @@ import MomentAdd from './src/screens/moment/MomentAdd';
 import MomentDetail from './src/screens/moment/MomentDetail';
 import AppHeader from './src/components/common/header/AppHeader';
 import StackHeader from './src/components/common/header/StackHeader';
-import Login from './src/screens/Login';
 import {StatusBar} from 'react-native';
 import {HomeStackParamList, MyPageStackParamList} from './src/types/screen';
-import AsyncStorage from '@react-native-async-storage/async-storage';
 import Login from './src/screens/Login';
 import Toast from 'react-native-toast-message';
 import {LinkingOptions} from '@react-navigation/native';
@@ -104,21 +101,21 @@ const StyledSafeAreaView = styled.SafeAreaView`
 `;
 
 const App = () => {
-  // const [isAuthenticated, setIsAuthenticated] = useState(true);
-  // const [loading, setLoading] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+  const [loading, setLoading] = useState(true);
 
-  // useEffect(() => {
-  //   const checkLoginStatus = async () => {
-  //     const token = await AsyncStorage.getItem('jwtToken');
-  //     setIsAuthenticated(!!token);
-  //     setLoading(false);
-  //   };
-  //   checkLoginStatus();
-  // }, []);
+  useEffect(() => {
+    const checkLoginStatus = async () => {
+      const token = await AsyncStorage.getItem('jwtToken');
+      setIsAuthenticated(!!token);
+      setLoading(false);
+    };
+    checkLoginStatus();
+  }, []);
 
-  // if (loading) {
-  //   return null; // 로딩 중일 때 빈 화면 또는 로딩 스피너를 보여줄 수 있습니다.
-  // }
+  if (loading) {
+    return null; // 로딩 중일 때 빈 화면 또는 로딩 스피너를 보여줄 수 있습니다.
+  }
 
   return (
     <ThemeProvider theme={theme}>
@@ -126,50 +123,50 @@ const App = () => {
       <StyledSafeAreaView>
         <NavigationContainer linking={linking}>
           <RootStack.Navigator>
-            {/* {isAuthenticated ? ( */}
-            <>
+            {isAuthenticated ? (
+              <>
+                <RootStack.Screen
+                  name="Bottom"
+                  component={TabNavigator}
+                  options={{headerShown: false}}
+                />
+                <RootStack.Screen
+                  name="Add"
+                  component={Add}
+                  options={() => ({
+                    header: () => <StackHeader title="모아 만들기" />,
+                    tabBarStyle: {display: 'none'},
+                  })}
+                />
+                <RootStack.Screen
+                  name="GroupAdd"
+                  component={GroupAdd}
+                  options={() => ({
+                    header: () => <StackHeader title="그룹 생성" />,
+                  })}
+                />
+                <RootStack.Screen
+                  name="MomentAdd"
+                  component={MomentAdd}
+                  options={() => ({
+                    header: () => <StackHeader title="순간 생성" />,
+                  })}
+                />
+                <RootStack.Screen
+                  name="Notification"
+                  component={Notification}
+                  options={{
+                    header: () => <StackHeader title="알림" />,
+                  }}
+                />
+              </>
+            ) : (
               <RootStack.Screen
-                name="Bottom"
-                component={TabNavigator}
-                options={{headerShown: false}}
-              />
-              <RootStack.Screen
-                name="Add"
-                component={Add}
-                options={() => ({
-                  header: () => <StackHeader title="모아 만들기" />,
-                  tabBarStyle: {display: 'none'},
-                })}
-              />
-              <RootStack.Screen
-                name="GroupAdd"
-                component={GroupAdd}
-                options={() => ({
-                  header: () => <StackHeader title="그룹 생성" />,
-                })}
-              />
-              <RootStack.Screen
-                name="MomentAdd"
-                component={MomentAdd}
-                options={() => ({
-                  header: () => <StackHeader title="순간 생성" />,
-                })}
-              />
-              <RootStack.Screen
-                name="Notification"
-                component={Notification}
-                options={{
-                  header: () => <StackHeader title="알림" />,
-                }}
-              />
-            </>
-            {/* ) : ( */}
-            {/* <RootStack.Screen
                 name="Login"
                 component={Login}
                 options={{headerShown: false}} // 로그인 화면에 헤더 숨기기
               />
-            )} */}
+            )}
             {/* 여기에 Screen 추가 */}
           </RootStack.Navigator>
         </NavigationContainer>
--- moa/src/components/common/login/LoginButton.tsx
+++ moa/src/components/common/login/LoginButton.tsx
@@ -1,30 +1,59 @@
 // src/components/common/login/LoginButton.tsx
-import React from 'react';
-import {Button, Alert} from 'react-native';
-import {login as kakaoLogin} from '@react-native-seoul/kakao-login';
+import React, {useState, useEffect} from 'react';
+import {Button, Alert, View} from 'react-native';
+import {login} from '@react-native-seoul/kakao-login';
+import styled from 'styled-components/native';
 
 type LoginButtonProps = {
   onLoginSuccess: (kakaoToken: string) => void;
 };
 
-const LoginButton: React.FC<LoginButtonProps> = ({onLoginSuccess}) => {
-  const handleKakaoLogin = async () => {
-    try {
-      const result = await kakaoLogin();
-      const kakaoToken = result.accessToken;
+const KakaoLoginButton = styled.Button`
+  width: 200px;
+  height: 50px;
+`;
 
-      if (kakaoToken) {
-        onLoginSuccess(kakaoToken);
+// const LoginButton: React.FC<LoginButtonProps> = ({onLoginSuccess}) => {
+const LoginButton = () => {
+  // const handleKakaoLogin = async () => {
+  //   try {
+  //     const result = await kakaoLogin();
+  //     const kakaoToken = result.accessToken;
+
+  //     if (kakaoToken) {
+  //       onLoginSuccess(kakaoToken);
+  //     } else {
+  //       throw new Error('카카오 토큰 발급 못받음!');
+  //     }
+  //   } catch (error) {
+  //     console.error('카카오 로그인 실패', error);
+  //     Alert.alert('로그인 실패', '다시 시도해주세용');
+  //   }
+  // };
+  const [result, setResult] = useState<string | null>(null);
+  const signInWithKakao = async () => {
+    try {
+      const token = await login();
+      const kakaoToken = token.accessToken;
+      setResult(`Kakao Login Success: ${JSON.stringify(token)}`);
+      if (token) {
+        console.log('토큰: ', result);
+        console.log('access토큰: ', token.accessToken);
+        // onLoginSuccess(kakaoToken);
       } else {
         throw new Error('카카오 토큰 발급 못받음!');
       }
     } catch (error) {
-      console.error('카카오 로그인 실패', error);
-      Alert.alert('로그인 실패', '다시 시도해주세용');
+      console.error('Login Failed', error);
+      Alert.alert('Login Failed', 'Please try again.');
     }
   };
 
-  return <Button title="Login with Kakao" onPress={handleKakaoLogin} />;
+  return (
+    <View>
+      <Button title="Login with Kakao" onPress={signInWithKakao} />;
+    </View>
+  );
 };
 
 export default LoginButton;
