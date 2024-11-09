ee91262b224bc7ca8cb5463f2328ab1d576a0912
김윤홍 <yunhongkim123@gmail.com>
2024-11-10 01:17:49 +0900

    :ambulance: [Fix] #S11P31A602-245 카카오 OAuth

--- backend/src/main/java/com/MOA/backend/global/auth/OAuth2/controller/KakaoOAuthController.java
+++ backend/src/main/java/com/MOA/backend/global/auth/OAuth2/controller/KakaoOAuthController.java
@@ -2,12 +2,14 @@ package com.MOA.backend.global.auth.OAuth2.controller;
 
 import com.MOA.backend.global.auth.OAuth2.service.KakaoOAuthService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/kakao")
@@ -16,13 +18,14 @@ public class KakaoOAuthController {
     private final KakaoOAuthService kakaoOAuthService;
 
     @PostMapping
-    public ResponseEntity<?> kakaoLogin(@RequestHeader("Authorization") String accessToken) {
+    public ResponseEntity<Map<String, String>> kakaoLogin(@RequestHeader("Authorization") String accessToken) {
         String token = accessToken.replace("Bearer ", "");
         String jwtToken = kakaoOAuthService.processUser(token);
-        HttpHeaders responseHeaders = new HttpHeaders();
-        responseHeaders.set("Authorization", "Bearer " + jwtToken);
-        return ResponseEntity.ok()
-                .headers(responseHeaders)
-                .body("로그인 성공");
+
+        Map<String, String> responseBody = new HashMap<>();
+        responseBody.put("accessToken", jwtToken);
+
+        return ResponseEntity.ok(responseBody);
     }
+
 }
