8f2a2660d377628f13d4f7db6d5d0579fa768068
dla9520 <dla9520@naver.com>
2024-11-14 15:02:40 +0900

    :sparkles:[Feat] #S11P31A602-233 #in-progress 그룹 상세 타입 추가

--- moa/src/types/group.ts
+++ moa/src/types/group.ts
@@ -9,18 +9,24 @@ export type GroupInfo = {
   memberCount: number;
 };
 
+export type Group = {
+  groupId: string;
+  groupPin: string;
+  groupName: string;
+  groupDescription: string;
+  groupIcon: string;
+  groupColor: string;
+  groupTotalImages: string;
+  createdAt: string;
+};
+
 export type GroupImages = {
   thumbImgs: Record<string, string[]>;
 };
 
 export type GroupInfoDetail = {
-  groupId: string;
-  groupName: string;
-  groupDescription: string;
-  groupColor: string;
-  groupIcon: string;
-  groupPin: string;
-  members: Member[];
+  group: Group;
+  users: Member[];
   groupOwner: Member;
   createdAt: string;
   images: GroupImages;
