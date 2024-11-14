97a0b082605a42b54aca195fe1ad19b140dc8dd0
dla9520 <dla9520@naver.com>
2024-11-14 13:09:03 +0900

    :sparkles:[Feat] #S11P31A602-171 #in-progress Group List 새로고침 구현

--- moa/src/components/group/groupList/MyGroupList.tsx
+++ moa/src/components/group/groupList/MyGroupList.tsx
@@ -11,53 +11,12 @@ const Container = styled.View`
   justify-content: space-between;
 `;
 
-const MyGroupList = () => {
-  // 테스트 데이터 (나중에 삭제 예정)
-  const groupList = [
-    {
-      groupId: '1',
-      groupName: '싸피 7반',
-      groupColor: 'red',
-      groupIcon: 'book-open',
-    },
-    {
-      groupId: '2',
-      groupName: '자율 602',
-      groupColor: 'yellow',
-      groupIcon: 'book-open',
-    },
-    {
-      groupId: '3',
-      groupName: '친구 모임',
-      groupColor: 'green',
-      groupIcon: 'chat',
-    },
-    {
-      groupId: '4',
-      groupName: '삼성전자',
-      groupColor: 'purple',
-      groupIcon: 'briefcase',
-    },
-    {
-      groupId: '7',
-      groupName: '동호회',
-      groupColor: 'blue',
-      groupIcon: 'chat',
-    },
-    {
-      groupId: '5',
-      groupName: '여자친구',
-      groupColor: 'pink',
-      groupIcon: 'heart',
-    },
-    {
-      groupId: '6',
-      groupName: '동호회입니다 오버플로우 테스트',
-      groupColor: 'blue',
-      groupIcon: 'chat',
-    },
-  ];
+interface MyGroupListProps {
+  refreshing: boolean;
+  onRefresh: () => void;
+}
 
+const MyGroupList = ({refreshing, onRefresh}: MyGroupListProps) => {
   return (
     <Container>
       {groupList.map((group) => (
