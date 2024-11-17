1127c5a4add025caf9172f6447a89d98868141a5
dla9520 <dla9520@naver.com>
2024-11-18 00:59:10 +0900

    :hammer:[Chore] 공유하기 앱 설정을 위한 AndroidManifest.xml, MainActivity.kt 설정 추가

--- moa/android/app/src/main/java/com/moa/MainActivity.kt
+++ moa/android/app/src/main/java/com/moa/MainActivity.kt
@@ -1,34 +1,55 @@
 package com.moa
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.modules.core.DeviceEventManagerModule
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
-import org.devio.rn.splashscreen.SplashScreen
 
 class MainActivity : ReactActivity() {
 
-    /**
-     * Returns the name of the main component registered from JavaScript.
-     * This is used to schedule rendering of the component.
-     */
     override fun getMainComponentName(): String = "moa"
 
-    /**
-     * Called when the activity is first created.
-     * This is where you initialize the SplashScreen.
-     */
     override fun onCreate(savedInstanceState: Bundle?) {
-        // SplashScreen.show(this)
         super.onCreate(savedInstanceState)
+        handleIntent(intent) // 앱이 실행될 때 Intent 데이터 처리
     }
 
-    /**
-     * Returns the instance of the [ReactActivityDelegate].
-     * We use [DefaultReactActivityDelegate] which allows you to enable
-     * New Architecture with a single boolean flag [fabricEnabled].
-     */
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        setIntent(intent)
+        handleIntent(intent) // 앱이 백그라운드에 있다가 새 Intent로 실행될 때 데이터 처리
+    }
+
+    private fun handleIntent(intent: Intent) {
+        when (intent.action) {
+            Intent.ACTION_SEND -> {
+                // 단일 이미지 처리
+                val imageUri: Uri? = intent.getParcelableExtra(Intent.EXTRA_STREAM)
+                if (imageUri != null) {
+                    sendEventToReactNative("ImageReceived", listOf(imageUri.toString()))
+                }
+            }
+            Intent.ACTION_SEND_MULTIPLE -> {
+                // 다중 이미지 처리
+                val imageUris: ArrayList<Uri>? = intent.getParcelableArrayListExtra(Intent.EXTRA_STREAM)
+                if (imageUris != null) {
+                    val uriStrings = imageUris.map { it.toString() }
+                    sendEventToReactNative("ImagesReceived", uriStrings)
+                }
+            }
+        }
+    }
+
+    private fun sendEventToReactNative(eventName: String, data: List<String>) {
+        val reactContext = reactInstanceManager.currentReactContext
+        reactContext?.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java)
+            ?.emit(eventName, data)
+    }
+
     override fun createReactActivityDelegate(): ReactActivityDelegate =
         DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
 }
