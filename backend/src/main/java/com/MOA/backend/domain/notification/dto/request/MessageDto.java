67cb07ea22a56cd035b612613d07f31e27320754
김윤홍 <yunhongkim123@gmail.com>
2024-11-18 18:36:09 +0900

    :sparkles: [Feat] back-feat fcm수정

--- backend/src/main/java/com/MOA/backend/domain/notification/dto/request/MessageDto.java
+++ backend/src/main/java/com/MOA/backend/domain/notification/dto/request/MessageDto.java
@@ -1,13 +1,23 @@
 package com.MOA.backend.domain.notification.dto.request;
 
-import lombok.AllArgsConstructor;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
 public class MessageDto {
     private Long groupId;
     private Long imageCount;
+
+    @JsonCreator
+    public MessageDto(
+            @JsonProperty("groupId") Long groupId,
+            @JsonProperty("imageCount") Long imageCount
+    ) {
+        this.groupId = groupId;
+        this.imageCount = imageCount;
+    }
 }
+
