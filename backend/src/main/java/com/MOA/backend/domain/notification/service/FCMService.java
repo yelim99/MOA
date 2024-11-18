80aa94c973a63cd48bff51ac452c655c21d70af8
김윤홍 <yunhongkim123@gmail.com>
2024-11-18 11:04:49 +0900

    :sparkles: [Feat] back-feat fcm 본인 제외 알림 처리

--- backend/src/main/java/com/MOA/backend/domain/notification/service/FCMService.java
+++ backend/src/main/java/com/MOA/backend/domain/notification/service/FCMService.java
@@ -14,11 +14,11 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.http.HttpHeaders;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
@@ -38,7 +38,7 @@ public class FCMService {
         User user = userRepository.findById(userId)
                 .orElseThrow(() -> new EntityNotFoundException("없는데요?"));
         Long groupId = messageDto.getGroupId();
-        String condition = String.format("'%s' in topics && !('%s' in topics)", groupId, userId);
+        String condition = String.format("'%s' in topics && !('%s' in topics)", groupId, user.getDeviceToken());
         String message = makeGroupMessage(user.getUserName(), messageDto, condition);
         log.info("+++++++{}", message);
         String accessToken = getAccessToken();
@@ -66,31 +66,44 @@ public class FCMService {
      *
      * @return 토큰
      */
+//    private String getAccessToken() {
+//        try {
+//            // 환경 변수로 Firebase 인증 파일 경로를 가져옵니다.
+//            String serviceAccountPath = System.getenv("GOOGLE_APPLICATION_CREDENTIALS");
+//            if (serviceAccountPath == null || serviceAccountPath.isEmpty()) {
+//                log.error("환경 변수 GOOGLE_APPLICATION_CREDENTIALS가 설정되지 않았습니다.");
+//                throw new IOException("환경 변수 GOOGLE_APPLICATION_CREDENTIALS가 설정되지 않았습니다.");
+//            }
+//
+//            log.info("Firebase 인증 파일을 환경 변수 경로에서 로드합니다: {}", serviceAccountPath);
+//
+//            // 환경 변수에 설정된 경로에서 파일 스트림을 생성
+//            try (FileInputStream serviceAccountStream = new FileInputStream(serviceAccountPath)) {
+//                GoogleCredentials googleCredentials = GoogleCredentials
+//                        .fromStream(serviceAccountStream)
+//                        .createScoped(List.of("https://www.googleapis.com/auth/cloud-platform"));
+//                googleCredentials.refreshIfExpired();
+//
+//                String accessToken = googleCredentials.getAccessToken().getTokenValue();
+//                log.info("getAccessToken() - 성공적으로 액세스 토큰을 가져왔습니다.");
+//                return accessToken;
+//            }
+//        } catch (IOException e) {
+//            log.error("Firebase 인증 파일을 읽는 중 오류 발생: {}", e.getMessage(), e);
+//            throw new RuntimeException("Firebase 인증 파일을 읽는데 실패했습니다.", e);
+//        }
+//    }
     private String getAccessToken() {
         try {
-            // 환경 변수로 Firebase 인증 파일 경로를 가져옵니다.
-            String serviceAccountPath = System.getenv("GOOGLE_APPLICATION_CREDENTIALS");
-            if (serviceAccountPath == null || serviceAccountPath.isEmpty()) {
-                log.error("환경 변수 GOOGLE_APPLICATION_CREDENTIALS가 설정되지 않았습니다.");
-                throw new IOException("환경 변수 GOOGLE_APPLICATION_CREDENTIALS가 설정되지 않았습니다.");
-            }
+            GoogleCredentials googleCredentials = GoogleCredentials
+                    .fromStream(new ClassPathResource("firebase-adminsdk.json").getInputStream())
+                    .createScoped(List.of("https://www.googleapis.com/auth/cloud-platform"));
+            googleCredentials.refreshIfExpired();
+            log.info("getAccessToken() - googleCredentials: {} ", googleCredentials.getAccessToken().getTokenValue());
 
-            log.info("Firebase 인증 파일을 환경 변수 경로에서 로드합니다: {}", serviceAccountPath);
-
-            // 환경 변수에 설정된 경로에서 파일 스트림을 생성
-            try (FileInputStream serviceAccountStream = new FileInputStream(serviceAccountPath)) {
-                GoogleCredentials googleCredentials = GoogleCredentials
-                        .fromStream(serviceAccountStream)
-                        .createScoped(List.of("https://www.googleapis.com/auth/cloud-platform"));
-                googleCredentials.refreshIfExpired();
-
-                String accessToken = googleCredentials.getAccessToken().getTokenValue();
-                log.info("getAccessToken() - 성공적으로 액세스 토큰을 가져왔습니다.");
-                return accessToken;
-            }
+            return googleCredentials.getAccessToken().getTokenValue();
         } catch (IOException e) {
-            log.error("Firebase 인증 파일을 읽는 중 오류 발생: {}", e.getMessage(), e);
-            throw new RuntimeException("Firebase 인증 파일을 읽는데 실패했습니다.", e);
+            throw new RuntimeException("파일을 읽는데 실패 했습니다.");
         }
     }
 
@@ -105,12 +118,10 @@ public class FCMService {
      */
     private String makeGroupMessage(String userName, MessageDto messageDto, String condition) throws JsonProcessingException {
         ObjectMapper om = new ObjectMapper();
-        Long groupId = messageDto.getGroupId();
         Long imageCount = messageDto.getImageCount();
         FCMMessage fcmMessage = FCMMessage
                 .builder()
                 .message(FCMMessage.Message.builder()
-                        .topic(groupId.toString())
                         .notification(FCMMessage.Notification.builder()
                                 .title("새로운 사진이 업로드되었습니다") // 고정된 제목
                                 .body(userName + "님이 그룹에 새로운 사진 " + imageCount + "장을 추가했습니다. 확인해보세요!") // 고정된 본문
